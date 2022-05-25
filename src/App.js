8019b748cf13f85e719db22715556de059554d30
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-05-25 14:25:36 -0400

    started creating components, and created folders for context and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from '@mantine/core';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
+
   return (
-    <div className="App">
-    </div>
+    <>
+    <BrowserRouter>
+    <Helmet>
+      <title>Voltaic - EV Charging Stations</title>
+      <meta name='title' content='Voltaic' />
+      <meta name='description' content='EV Charging Stations' />
+      <meta property="og:title" content='Voltaic - EV Charging Stations' />
+      <meta property="og:description" content='EV Charging Stations' />
+      <meta property="og:image" content='%PUBLIC_URL%/preview.png' />
+    </Helmet>
+    <Container size="lg">
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    </Container>
+    </BrowserRouter>
+    </>
   );
 }
 
-export default App;
+export default App
