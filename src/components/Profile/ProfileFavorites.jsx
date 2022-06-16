6a086e74701a161bfabd28809c7e361e7229e4ad
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-06-16 14:19:47 -0400

    removed Link from button because it is no longer needed as the button will just load more stations

--- src/components/Profile/ProfileFavorites.jsx
+++ src/components/Profile/ProfileFavorites.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Title, Button } from '@mantine/core';
-import { Link } from 'react-router-dom';
 import UsersFavStations from './UsersFavStations';
 
 const ProfileFavorites = () => {
@@ -10,9 +9,7 @@ const ProfileFavorites = () => {
       <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auto', marginTop: '10px' }}><Title order={3}>Austin's Favorite Stations</Title></div>
       <UsersFavStations />
       <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
-        <Link to='/user/austin/favorites'>
-          <Button variant="light" size="sm" color="indigo">Load More</Button>
-        </Link>
+        <Button variant="light" size="sm" color="indigo">Load More</Button>
       </div>
     </>
   )
--- src/components/Profile/ProfileStations.jsx
+++ src/components/Profile/ProfileStations.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Title, Button } from '@mantine/core';
-import { Link } from 'react-router-dom';
 import UsersStations from './UsersStations';
 
 const ProfileStations = () => {
@@ -9,9 +8,7 @@ const ProfileStations = () => {
       <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auto', marginTop: '10px' }}><Title order={3}>Austin's Hosted Charging Stations</Title></div>
       <UsersStations />
       <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
-        <Link to='/user/austin/favorites'>
-          <Button variant="light" size="sm" color="indigo">Load More</Button>
-        </Link>
+        <Button variant="light" size="sm" color="indigo">Load More</Button>
       </div>
     </>
   )
