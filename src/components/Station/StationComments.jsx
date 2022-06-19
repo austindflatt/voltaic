c386379a953a0323fdbebd4011fa390a5b37dd1b
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-06-19 13:23:59 -0400

    created comments UI

--- src/components/Station/StationComments.jsx
+++ src/components/Station/StationComments.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { createStyles, Text, Avatar, Group } from '@mantine/core';
 
-const StationComments = () => {
+const useStyles = createStyles((theme) => ({
+  body: {
+    paddingLeft: 54,
+    paddingTop: theme.spacing.sm,
+  },
+}));
+
+function StationComments() {
+  const { classes } = useStyles();
   return (
-	  <div>StationComments</div>
-  )
+    <div>
+      <Group>
+        <Avatar src='https://www.austinflatt.com/images/headshot.webp' alt='Austin' radius="xl" />
+        <div>
+          <Text size="sm">Austin</Text>
+          <Text size="xs" color="dimmed">
+            Posted 10 mins ago
+          </Text>
+        </div>
+      </Group>
+      <Text className={classes.body} size="sm">
+        This is a review for this station
+      </Text>
+    </div>
+  );
 }
 
 export default StationComments
--- src/pages/Station/StationPage.jsx
+++ src/pages/Station/StationPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import StationComments from '../../components/Station/StationComments'
 
 const StationPage = () => {
   return (
     <Layout>
-      
+      <StationComments />
     </Layout>
   )
 }
